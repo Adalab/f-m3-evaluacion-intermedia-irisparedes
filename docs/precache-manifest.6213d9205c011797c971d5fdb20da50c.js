87874701d9a7e14b20ec02c78c04b21d3bb36653
iris paredes <irisparedesjimenez@gmail.com>
2019-05-19 17:01:55 +0200

    add docs folder

---
+++ docs/precache-manifest.6213d9205c011797c971d5fdb20da50c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66c65d4475c34d7064f8b7d356739817",
+    "url": "./index.html"
+  },
+  {
+    "revision": "591aa00dd9af86a3dc18",
+    "url": "./static/css/main.70e62d1e.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "./static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "591aa00dd9af86a3dc18",
+    "url": "./static/js/main.b9e7e185.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
